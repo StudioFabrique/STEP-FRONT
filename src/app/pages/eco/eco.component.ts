892acdc1aa2186a27b979a739c5d9435cfe6f4ae
alaa <aahmad@fabriquenumerique.fr>
2021-10-26 10:32:58 +0200

    page eco

--- src/app/pages/eco/eco.component.ts
+++ src/app/pages/eco/eco.component.ts
@@ -7,10 +7,10 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class EcoComponent implements OnInit {
 
-  @Input() image: string = 'default';
-  @Input() title: string = 'default'
-  @Input() titleBold: string = 'default'
-  @Input() text: string = 'default'
+  
+  @Input() title: string = ''
+  @Input() titleBold: string = 'Une aventure, un état d’esprit !'
+  @Input() text: string = 'Les questions sociétales, en termes de responsabilité sociale des entreprises (RSE) et environnementales sont intégrées dans chacune de nos activités. Ainsi nous œuvrons au maximum pour limiter les impacts des développements informatiques sur l’environnement en aidant nos clients à repenser leurs usages et à choisir la technologie la plus adaptée, en appliquant ces principes en interne et en misant sur l’éco conception.'
   constructor() { }
 
   ngOnInit(): void {
--- src/app/templates/eco-header/eco-header.component.ts
+++ src/app/templates/eco-header/eco-header.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 
 export interface Model {
-  image: string,
+
   title: string,
   titleBold: string,
   text: string
@@ -15,10 +15,10 @@ export interface Model {
 })
 export class EcoHeaderComponent implements OnInit {
 
-  @Input() image: string = 'default';
-@Input() title: string = 'default'
-@Input() titleBold: string = 'default'
-@Input() text: string = 'default'
+
+  @Input() title: string = 'default'
+  @Input() titleBold: string = 'default'
+  @Input() text: string = 'default'
 
   constructor() { }
 
