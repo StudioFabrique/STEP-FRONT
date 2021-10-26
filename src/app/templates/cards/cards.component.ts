627986736b257c569d0e6dc44f4da937f913cf73
alaa <aahmad@fabriquenumerique.fr>
2021-10-26 14:21:05 +0200

    card changement

--- src/app/templates/cards/cards.component.ts
+++ src/app/templates/cards/cards.component.ts
@@ -28,17 +28,17 @@ export class CardsComponent implements OnInit {
   cards: Card[] = [
     {
       'content': 'Depuis 20 ans, l’ADN de STEP - entreprise citoyenne et responsable- c’est de construire des parcours inclusifs, par la production de services innovants, ou par la formation, à destination des entreprises et des collectivités.',
-     'icon':'hub',
+     'icon':'chat_bubble_outline',
       'title': 'Une entreprise citoyenne...'
     }, 
     {
       'content': 'Nous ne sommes pas nés d’hier et nous savons que demain la solidarité sera la norme. Agir au service des territoires, donner la chance d’avoir une seconde chance – et même une première chance – est notre métier.',
-      'icon':'hub',
+      'icon':'chat_bubble_outline',
       'title': '...au service du territoire'
     }, 
     {
       'content': 'STEP veut être une passerelle vers l’emploi durable, et surtout la confiance retrouvée.', 
-    'icon':'hub', 
+    'icon':'chat_bubble_outline', 
     'title': 'Une passerelle vers l’emploi et la confiance retrouvée !'
     }
   ]
