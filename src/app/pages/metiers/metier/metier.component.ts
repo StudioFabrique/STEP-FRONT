19e1590bb1e5f77b1b9bc80467d9714df2e8424b
aahmad-fabriqueNumerique <aahmad@fabriqueNumerique.fr>
2021-11-25 11:35:50 +0100

    metier header modifs

--- src/app/pages/metiers/metier/metier.component.ts
+++ src/app/pages/metiers/metier/metier.component.ts
@@ -17,10 +17,11 @@ export interface Content {
 export class MetierComponent implements OnInit {
 
 
-  image="assets/img/corner-orange.png" 
+  cornerImg="assets/img/corner-orange.png" 
   title="Des solutions et des métiers " 
   titleBold="adaptés à vos besoins" 
   text="STEP vous accompagne en vous proposant des solutions globales et ses expertises dans plusieurs grands secteurs d’activités. Les flux documentaires et le back office, les services de territoire et l’ingénierie informatique." 
+  bg="../../../assets/img/solutions.png"
 
   
 
--- src/app/shared/header-color-block/header-color-block.component.ts
+++ src/app/shared/header-color-block/header-color-block.component.ts
@@ -1,17 +1,19 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header-color-block',
   templateUrl: './header-color-block.component.html',
   styleUrls: ['./header-color-block.component.css'],
 })
-export class HeaderColorBlockComponent implements OnInit {
+export class HeaderColorBlockComponent  implements OnInit {
   
-  @Input() image: string = 'default'
-  @Input() title: string = 'default'
-  @Input() titleBold: string = 'default'
-  @Input() text: string = 'default'
+  // bg = '../../../assets/img/solutions.png';
   
+  @Input() cornerImg: string = 'default';
+  @Input() title: string = 'default';
+  @Input() titleBold: string = 'default';
+  @Input() text: string = 'default';
+  @Input() bg: string = '../../../assets/img/solutions.png';
   
   constructor() {}
   ngOnInit(): void {}
